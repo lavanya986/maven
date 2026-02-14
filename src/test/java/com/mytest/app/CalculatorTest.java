520d7ad660746276314392a28f298ca484e997ae
lavanya986 <lavanyam986@gmail.com>
2026-02-14 10:14:11 +0530

    Create CalculatorTest.java

---
+++ src/test/java/com/mytest/app/CalculatorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Test;
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+}
